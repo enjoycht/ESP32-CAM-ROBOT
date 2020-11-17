1dcb45ff05cf9d791e2a8ff21a5c943b3eaedc69
enjoycht <enjoycht@gmail.com>
2020-11-17 15:00:04 +0800

    Update ESP32CamRobot.ino
    SERVO_CH & SERVO_Freq to instead of value 4 & 50.
    Correct the mistake of "value" in function:
    void servoWrite (.........

--- ESP32CamRobot/ESP32CamRobot.ino
+++ ESP32CamRobot/ESP32CamRobot.ino
@@ -26,6 +26,51 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+
+	The UI web page is located at "data" folder, and it must be upload in to SPIFFS via SPIFFS upload tool.
+	Download the tools at: https://github.com/me-no-dev/arduino-esp32fs-plugin/releases/
+	and extract in arduino ide folder/tools. So the folder will have this new ESP32FS folder added.
+	my example: an old howto.txt file, 2 old folder + this new ESP32FS folder as shown :
+		Mangler
+		WiFi101
+		ESP32FS
+		howto.txt
+
+	Restart the IDE to active the ESP32 SPIFFS tool, ( refer to the vide show how to upload at 06:31 )
+	How to upload the firmware:
+
+    ESP32-CAM module firmware(ESP32CamRobot): contains 2 parts
+
+    data: contains the GUI (html, icon), use SPIFFS upload tool to upload to the module
+    ESP32CamRobot sketch: normally upload by arduino IDE
+
+    Arduino Uno firmware (ESP32CamRobotMotors): normally upload by arduino IDE
+
+	How to run:
+
+		Use Arduino IDE serial monitor tool to capture the serial data from ESP32-CAM module
+		on starting up to get the camera IP address.
+		Use your webbrowser to connect to the that IP.
+
+	Change the LED pin from 2 to the built in LED pin ( #4 ).
+
+	Test to approve and Note by enjoycht 202-11-16
+
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	The board manager MUST pick "ESP32 Wrover Module", the data file uploader & functions "SPIFFS.h"
+	won't work with "Ai Thinker ESP32-CAM". So evenwhen uploading the arduino code, pick "ESP32 Wrover Module".
+
+	The Serial port console must be shut off when uploading the data or firmware, or
+	Once when the board manager selection changed or after upload SPIFFS, MUST RESTART THE ARDUINO IDE.
+
+	Add const int "SERVO_CH" & "SERVO_Freq" to instead of values "4" & "50".
+
+	by enjoycht 2020-11-17
+
+*/
+
 #include <WebSocketsServer.h>
 #include <WiFi.h>
 #include <SPIFFS.h>
@@ -49,9 +94,12 @@ WebSocketsServer webSocket = WebSocketsServer(86);
 AsyncWebServer httpServer(80);
 String html_home;
 
-const int PIN_SERVO   = 12;
-const int PIN_LED     = 2;
-const int SERVO_RESOLUTION    = 16;
+const int PIN_LED = 4 ;														// The built in LED of ESP32-CAM is connected to GPIO4
+
+const int PIN_SERVO = 12 ;
+const int SERVO_CH = 4 ;													// Define the servo is Channel 4
+const int SERVO_RESOLUTION = 16 ;
+const int SERVO_Freq = 50 ;												// Servo update frequency 50Hz = 20 ms.
 unsigned long previousMillisServo = 0;
 const unsigned long intervalServo = 100;
 bool reqUp = false;
@@ -166,13 +214,13 @@ void setup(void) {
   // 1. 50hz ==> period = 20ms (sg90 servo require 20ms pulse, duty cycle is 1->2ms: -90=>90degree)
   // 2. resolution = 16, maximum value is 2^16-1=65535
   // From 1 and 2 => -90=>90 degree or 0=>180degree ~ 3276=>6553
-  ledcSetup(4, 50, SERVO_RESOLUTION);//channel, freq, resolution
-  ledcAttachPin(PIN_SERVO, 4);// pin, channel
+  ledcSetup ( SERVO_CH , SERVO_Freq , SERVO_RESOLUTION ) ;	         // channel , freq , resolution, modification 2020-11-18
+  ledcAttachPin ( PIN_SERVO , SERVO_CH ) ;									// pin , channel, modification 2020-11-18
 
   pinMode(PIN_LED, OUTPUT);
 }
 
-void servoWrite(uint8_t channel, uint8_t value) {
+void servoWrite ( uint8_t channel , uint8_t angle ) {               // Correction on date 2020-11-17
   // regarding the datasheet of sg90 servo, pwm period is 20 ms and duty is 1->2ms
   uint32_t maxDuty = (pow(2,SERVO_RESOLUTION)-1)/10; 
   uint32_t minDuty = (pow(2,SERVO_RESOLUTION)-1)/20; 
@@ -192,7 +240,7 @@ void controlServo(){
     }
   }
   
-  servoWrite(4,posServo);
+  servoWrite ( SERVO_CH , posServo ) ;                            // modification 2020-11-18
 }
 
 void commadProcessing(AsyncWebServerRequest *request){
